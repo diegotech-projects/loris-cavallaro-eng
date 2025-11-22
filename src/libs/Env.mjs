d329ee3f69ab1347873a2c37a24934f12931119b
Diego Margoni <diegof.university@gmail.com>
2025-11-22 23:03:30 +0100

    feat: add environment variables for site configuration and contact details

--- src/libs/Env.mjs
+++ src/libs/Env.mjs
@@ -5,28 +5,35 @@ import { z } from 'zod';
 // Don't add NODE_ENV into T3 Env, it changes the tree-shaking behavior
 export const Env = createEnv({
   server: {
-    CLERK_SECRET_KEY: z.string().nonempty(),
-    DATABASE_URL: z.string().nonempty(),
-    DATABASE_AUTH_TOKEN: z.string().optional(),
   },
   client: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().nonempty(),
-    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().nonempty(),
-    NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().nonempty(),
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().nonempty(),
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().nonempty(),
+    NEXT_PUBLIC_SITE_URL: z.string().url(),
+    NEXT_PUBLIC_NOME_COGNOME: z.string().nonempty(),
+    NEXT_PUBLIC_SIGLA: z.string().nonempty(),
+    NEXT_PUBLIC_LATITUDINE: z.string().nonempty(),
+    NEXT_PUBLIC_LONGITUDINE: z.string().nonempty(),
+    NEXT_PUBLIC_ZOOM: z.string().nonempty(),
+    NEXT_PUBLIC_COMPANY_EMAIL: z.string().email(),
+    NEXT_PUBLIC_COMPANY_EMAIL_SECONDARY: z.string().email(),
+    NEXT_PUBLIC_COMPANY_PHONE: z.string().nonempty(),
+    NEXT_PUBLIC_COMPANY_NAME: z.string().nonempty(),
+    NEXT_PUBLIC_ENGINEER_NAME: z.string().nonempty(),
+    NEXT_PUBLIC_VAT_NUMBER: z.string().nonempty(),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     DATABASE_AUTH_TOKEN: process.env.DATABASE_AUTH_TOKEN,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
-    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL:
-      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL:
-      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    NEXT_PUBLIC_NOME_COGNOME: process.env.NEXT_PUBLIC_NOME_COGNOME,
+    NEXT_PUBLIC_SIGLA: process.env.NEXT_PUBLIC_SIGLA,
+    NEXT_PUBLIC_LATITUDINE: process.env.NEXT_PUBLIC_LATITUDINE,
+    NEXT_PUBLIC_LONGITUDINE: process.env.NEXT_PUBLIC_LONGITUDINE,
+    NEXT_PUBLIC_ZOOM: process.env.NEXT_PUBLIC_ZOOM,
+    NEXT_PUBLIC_COMPANY_EMAIL: process.env.NEXT_PUBLIC_COMPANY_EMAIL,
+    NEXT_PUBLIC_COMPANY_EMAIL_SECONDARY: process.env.NEXT_PUBLIC_COMPANY_EMAIL_SECONDARY,
+    NEXT_PUBLIC_COMPANY_PHONE: process.env.NEXT_PUBLIC_COMPANY_PHONE,
+    NEXT_PUBLIC_COMPANY_NAME: process.env.NEXT_PUBLIC_COMPANY_NAME,
+    NEXT_PUBLIC_ENGINEER_NAME: process.env.NEXT_PUBLIC_ENGINEER_NAME,
+    NEXT_PUBLIC_VAT_NUMBER: process.env.NEXT_PUBLIC_VAT_NUMBER,
   },
 });
